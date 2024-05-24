4c6b95731654c65d1a5e270b76bbdfc1bfa3d8cb
nitish <ns20580721@gmail.com>
2024-05-24 09:32:30 +0545

    sign and signup page added

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+     path('', views.signin, name='signin'),
+      path('', views.signup, name='signup'),
+    
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def signin(request):
+    return render(request, 'signin.html')
+def signup(request):
+    return render(request, 'signup.html')
